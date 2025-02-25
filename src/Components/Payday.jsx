4b6781b1b014ad091a9630a85d729e071fef6e99
Folakl <yakubroimot2001@gmail.com>
2025-02-25 02:48:05 -0800

    made adjustment

--- src/Components/Payday.jsx
+++ src/Components/Payday.jsx
@@ -8,12 +8,12 @@ const Payday = () => {
         <div>
           <img src={one} className='w-[650px] h-[300px]' alt="" />
         </div>
-        <div className='p-20'>
-          <h3 className='bg-white text-black text-[40px] px-5 font-bold'>PAYDAY</h3>
+        <div className='p-[50px]'>
+          <h3 className='bg-white text-black text-[40px] px-10 font-bold'>PAYDAY</h3>
           <h3 className='text-[40px] font-bold'> SALE NOW</h3>
-          <h3>Spend minimal $100 get 30% off voucher <br /> code for your next purchase</h3>
-          <h3 className='font-bold my-5'>1 June - 10 June 2021</h3>
-          <h3 className='my-5'>*Terms & Conditions apply</h3>
+          <h3>Spend minimal $100 get 30% off voucher  code for your next purchase</h3>
+          <h3 className='font-bold my-3'>1 June - 10 June 2021</h3>
+          <h3 className='my-3'>*Terms & Conditions apply</h3>
 
           <button className='bg-black h-[50px] my-10  font-bold w-[150px] text-white'>SHOP NOW</button>
         </div>
